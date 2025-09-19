8283516a73a20ac2caa82fdbb1abffd20fd4fb05
DIlanka Guruhitha <dsandun73@gmail.com>
2025-09-19 12:50:22 +0530

    db first

---
+++ financial-site/dashboard.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+</head>
+
+<body>
+    <div class="sidebar">
+        <ul>
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Clients</a></li>
+            <li><a href="#">Messages</a></li>
+            <li><a href="#">Reports</a></li>
+        </ul>
+    </div>
+    <div class="header">
+        <div class="user-profile"></div>
+        <div class="search"></div>
+        <div class="notifications"></div>
+        <div class="settings"><button class="export">Export</button></div>
+    </div>
+    <div class="main">
+        <div class="user-message">
+            <div class="cards">
+                <div class="card"></div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
